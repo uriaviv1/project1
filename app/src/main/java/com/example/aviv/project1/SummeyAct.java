80ce076764761b2abc988061458d5f6c16963fa5
uriaviv1 <31965368+uriaviv1@users.noreply.github.com>
2018-02-13 00:30:10 +0200

    menu is working

--- app/src/main/java/com/example/aviv/project1/SummeyAct.java
+++ app/src/main/java/com/example/aviv/project1/SummeyAct.java
@@ -2,10 +2,13 @@ package com.example.aviv.project1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ActionMenuView;
@@ -15,48 +18,42 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SummeyAct extends AppCompatActivity {
+public class SummeyAct extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     Context context;
+    NavigationView navigationView;
     FirebaseAuth firebaseAuth;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-
-    Button moveToPlayersActivity, logOut, gotochatactivity;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summey);
+        firebaseAuth=FirebaseAuth.getInstance();
+        navigationView=(NavigationView)findViewById(R.id.navigation_menu);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this ,drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
         context = this;
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        actionBarDrawerToggle.onOptionsItemSelected(item);
+
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
            // return true;
-
-        switch (item.getItemId()) {
-            case R.id.PlayerListMenu:
-                Intent intent = new Intent(context, Players.class);
-                startActivity(intent);
-                return true;
-            case R.id.chatMenu:
-                Intent intent2 = new Intent(context, Players.class);
-                startActivity(intent2);
-                return true;
-            case R.id.LogOutMenu:
-                LogOutMethod();
-                return true;
-
-
         }
 
         return super.onOptionsItemSelected(item);
@@ -69,4 +66,24 @@ public class SummeyAct extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d("menu id",String.valueOf(item.getItemId()));
+        switch (item.getItemId()) {
+            case R.id.PlayerListMenu:
+                Intent intent = new Intent(context, Players.class);
+                startActivity(intent);
+                break;
+            case R.id.FanChatMenu:
+                Intent intent2 = new Intent(context,Chatactivity.class);
+                startActivity(intent2);
+                break;
+                case R.id.SighnOutMenu:
+                LogOutMethod();
+                break;
+
+
+        }
+        return false;
+    }
 }
