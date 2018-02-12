075482ca7a2f06fe1599eba76b8c24656c1da2d0
uriaviv1 <31965368+uriaviv1@users.noreply.github.com>
2018-02-12 22:18:07 +0200

    navigation menu

--- app/src/main/java/com/example/aviv/project1/SummeyAct.java
+++ app/src/main/java/com/example/aviv/project1/SummeyAct.java
@@ -2,53 +2,71 @@ package com.example.aviv.project1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ActionMenuView;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SummeyAct extends AppCompatActivity implements View.OnClickListener {
-Context context;
-FirebaseAuth firebaseAuth;
+public class SummeyAct extends AppCompatActivity {
+    Context context;
+    FirebaseAuth firebaseAuth;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
-    Button moveToPlayersActivity,logOut,gotochatactivity;
-Intent intent;
+    Button moveToPlayersActivity, logOut, gotochatactivity;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summey);
-    moveToPlayersActivity=(Button)findViewById(R.id.goToPlayersActivity);
-    moveToPlayersActivity.setOnClickListener(this);
-    logOut=(Button)findViewById(R.id.logOut);
-    logOut.setOnClickListener(this);
-    firebaseAuth=FirebaseAuth.getInstance();
-    gotochatactivity=(Button)findViewById(R.id.gotochatactivity);
-    gotochatactivity.setOnClickListener(this);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    context=this;
+
+        context = this;
     }
 
     @Override
-    public void onClick(View view) {
-        if(view==moveToPlayersActivity) {
-            intent = new Intent(this, Players.class);
-            startActivity(intent);
-        }
-        if (view==logOut)
-        {
-            firebaseAuth.signOut();
-            intent=new Intent(this,MainActivity.class);
-            startActivity(intent);
-        }
-        if(view==gotochatactivity)
-        {
-            intent=new Intent(context,Chatactivity.class);
-            startActivity(intent);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        actionBarDrawerToggle.onOptionsItemSelected(item);
+           // return true;
+
+        switch (item.getItemId()) {
+            case R.id.PlayerListMenu:
+                Intent intent = new Intent(context, Players.class);
+                startActivity(intent);
+                return true;
+            case R.id.chatMenu:
+                Intent intent2 = new Intent(context, Players.class);
+                startActivity(intent2);
+                return true;
+            case R.id.LogOutMenu:
+                LogOutMethod();
+                return true;
+
 
         }
+
+        return super.onOptionsItemSelected(item);
     }
+
+    private void LogOutMethod() {
+        firebaseAuth.signOut();
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
